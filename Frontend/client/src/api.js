b3c87d94d24baaa2bbb51889cf66a8a375473321
Aman Ruhela <amank004433@gmail.com>
2026-01-13 22:39:34 +0530

    Update api.js

--- Frontend/client/src/api.js
+++ Frontend/client/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL =  "http://localhost:3000/api/v2/"
+const BASE_URL =  "https://expense-tracker-backend-0sjt.onrender.com"
 
 const api = axios.create({baseURL : BASE_URL, timeout: 8000})
 
