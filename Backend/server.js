e65f0d49dc31fa491ceef3a2c23f52f00da27d5c
Aman Ruhela <amank004433@gmail.com>
2026-01-17 19:58:53 +0530

    fixing cors error

--- Backend/server.js
+++ Backend/server.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 8000
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin:['http://localhost:5173','https://expensetracker-mmel.onrender.com']
+    origin:'https://expensetracker-mmel.onrender.com'
 }))
 
 dbConnection()
