3eb5a8a2a7992c7c9a6f476869aa66f9f98c0d32
Aman Ruhela <amank004433@gmail.com>
2026-03-11 21:46:34 +0530

    remove localhost

--- Backend/server.js
+++ Backend/server.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 8000
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin: ['https://expense-tracker-5vz4.onrender.com', 'http://localhost:5173','http://localhost:5174']
+    origin: ['https://expense-tracker-5vz4.onrender.com']
 }))
 
 dbConnection()
